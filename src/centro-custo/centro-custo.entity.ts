fb510389053c4e021eac5f3a7567a451ba82179e
olivir de paula junior <owlauch@hotmail.com>
2020-12-28 11:19:13 -0300

    ajustes

--- src/centro-custo/centro-custo.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-@Entity()
-export class CentroCusto {
-  @PrimaryGeneratedColumn()
-  id: number;
-  @Column()
-  descricao: string;
-}
